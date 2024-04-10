62056a206cb6f02c1b20cf6b029e85ef3aebb957
YuanSamuel <syuan@utexas.edu>
2024-04-09 20:13:33 -0500

    glider

---
+++ sim/inc/glider_predictor.h
@@ -0,0 +1,185 @@
+#ifndef GLIDER_PREDICTOR_H
+#define GLIDER_PREDICTOR_H
+
+using namespace std;
+#include <map>
+#include <numeric>
+#include <vector>
+
+#include "helper_function.h"
+#include "optgen.h"
+
+#define MAX_PCMAP 31
+#define PCMAP_SIZE 16 // Assume 16
+
+// Constants
+#define GLIDER_THRESHOLD_HIGH 60 // Prediction threshold
+#define GLIDER_THRESHOLD_LOW 0
+
+enum class Prediction { High, Medium, Low };
+
+class IntegerSVM
+{
+private:
+  vector<int> weights;
+
+public:
+  IntegerSVM(int num_weights) { weights.resize(num_weights, 0); }
+
+  void update_weights(const vector<uint64_t>& indices, int should_cache, int thresholds, int learning_rate = 1, int lambda = 1,
+                      int regularization_threshold = 5)
+  {
+    // Calculate the sum of the weights
+    int weight_sum = accumulate(indices.begin(), indices.end(), 0, [this](int sum, int idx) { return sum + this->weights[idx]; });
+
+    // Check if the threshold is exceeded
+    if (abs(weight_sum) >= thresholds) {
+      return; // Do not update weights if exceeded
+    }
+
+    // Update weights based off OPTGen decisions
+    for (auto idx : indices) {
+      weights[idx] += learning_rate * should_cache;
+    }
+
+    // Step regularization
+    for (auto& weight : weights) {
+      if (abs(weight) > regularization_threshold) {
+        if (weight > 0) {
+          weight -= lambda;
+        } else {
+          weight += lambda;
+        }
+      }
+    }
+  }
+
+  // Cakcykate the sum of weights based on the PCHR index
+  int calculate_weights(vector<uint64_t> pchr)
+  {
+    int weight_sum = 0;
+    for (uint64_t pc : pchr) {
+      weight_sum += weights[pc];
+    }
+    return weight_sum;
+  }
+
+  // Calculate the sum of all weights
+  int calculate_sum() const { return accumulate(weights.begin(), weights.end(), 0); }
+
+  Prediction predict(const vector<int>& indices)
+  {
+    // Calcualte predicted value
+    int prediction = accumulate(indices.begin(), indices.end(), 0, [this](int sum, int idx) { return sum + this->weights[idx]; });
+    return prediction >= 60 ? Prediction::High : prediction >= 0 ? Prediction::Medium : Prediction::Low;
+  }
+
+  // Print weights (debug)
+  void print_weights() const
+  {
+    for (int weight : weights) {
+      cout << weight << " ";
+    }
+    cout << endl;
+  }
+};
+
+class Glider_Predictor
+{
+private:
+  vector<uint64_t> pchr;                                          // PCHR history
+  const int k_sparse = 5;                                         // PCHR max length
+  const vector<int> dynamic_thresholds = {0, 30, 100, 300, 3000}; // Dynamic thresholds
+  vector<IntegerSVM> isvms;
+
+public:
+  // Init OPTGen and PCHR
+  Glider_Predictor()
+  {
+    pchr.resize(k_sparse, 0);
+
+    for (int i = 0; i < PCMAP_SIZE; i++) {
+      isvms.push_back(IntegerSVM(PCMAP_SIZE));
+    }
+  }
+
+  // Dynamically adjust thresholds
+  int select_dynamic_threshold()
+  {
+    // Sum current weights in ISVM
+    int weight_sum = 0;
+    for (const auto& svm : isvms) {
+      weight_sum += svm.calculate_sum();
+    }
+
+    // Choose approx. threshold based on sum of weights
+    size_t index;
+    if (weight_sum < 500) {
+      index = 4;
+    } else if (weight_sum < 1000) {
+      index = 3;
+    } else if (weight_sum < 2000) {
+      index = 2;
+    } else if (weight_sum < 4000) {
+      index = 1;
+    } else {
+      index = 0;
+    }
+    return dynamic_thresholds[index];
+  }
+
+  // Return prediction results
+  Prediction get_prediction(uint64_t PC)
+  {
+    // Push PC to end of PCHR history, pop beginning
+    uint64_t encoded_pc = CRC(PC) % PCMAP_SIZE; // CRC hash with mod 16
+    pchr.push_back(encoded_pc);
+    if (pchr.size() > (size_t)k_sparse) {
+      pchr.erase(pchr.begin());
+    }
+
+    int weight_sum = isvms[encoded_pc].calculate_weights(pchr);
+
+    if (weight_sum >= GLIDER_THRESHOLD_HIGH) {
+      return Prediction::High;
+    } else if (weight_sum < GLIDER_THRESHOLD_LOW) {
+      return Prediction::Low;
+    }
+    return Prediction::Medium;
+  }
+
+  void increase(uint64_t PC)
+  {
+    uint64_t encoded_pc = CRC(PC) % PCMAP_SIZE;
+
+    // Choose appropriate dynamic threshold
+    int selected_threshold = select_dynamic_threshold();
+
+    isvms[encoded_pc].update_weights(pchr, 1, selected_threshold);
+  }
+
+  void decrease(uint64_t PC)
+  {
+    uint64_t encoded_pc = CRC(PC) % PCMAP_SIZE;
+
+    // Choose appropriate dynamic threshold
+    int selected_threshold = select_dynamic_threshold();
+
+    isvms[encoded_pc].update_weights(pchr, -1, selected_threshold);
+  }
+
+  // Print all weights of all ISVMs
+  void print_all_weights() const
+  {
+    cout << "Printing all SVM weights:" << endl;
+    int svm_index = 1;
+    for (const auto& svm : isvms) {
+      cout << "SVM #" << svm_index << ": ";
+      svm.print_weights();
+      svm_index++;
+    }
+    cout << "----------" << endl;
+  }
+};
+
+#endif
---
+++ sim/replacement/glider/glider.cc
@@ -0,0 +1,366 @@
+#include <map>
+#include <math.h>
+
+#include "cache.h"
+
+// Num cores in the system
+#define NUM_CORE 1
+// Total num LLC sets
+#define LLC_SETS NUM_CORE * 2048
+// Num ways in each set
+#define LLC_WAYS 16
+
+// RRIP - 
+#define MAXRRIP 7
+#define MIDRRIP 2
+
+// Store RRIP value for each way for each set
+uint32_t rrpv[LLC_SETS][LLC_WAYS];
+
+#define OPTGEN_VECTOR_SIZE 128
+
+struct rrpv_structure
+{
+    int way;
+    int rrpv_val;
+    bool flag;
+};
+
+struct rrpv_structure rrpv_str[LLC_WAYS];
+
+
+
+#include <iostream>
+
+#include "glider_predictor.h"
+#include "helper_function.h"
+#include "optgen.h"
+
+// Glider predictor
+Glider_Predictor *demand_predictor;
+Glider_Predictor *prefetch_predictor;
+
+//Per-set timers; we only use 64 of these
+//Budget = 64 sets * 1 timer per set * 10 bits per timer = 80 bytes
+#define TIMER_SIZE 1024
+uint64_t perset_mytimer[LLC_SETS];
+
+// Signatures for sampled sets; we only use 64 of these
+// Budget = 64 sets * 16 ways * 12-bit signature per line = 1.5B
+uint64_t signatures[LLC_SETS][LLC_WAYS];
+bool prefetched[LLC_SETS][LLC_WAYS];
+
+#define OPTGEN_VECTOR_SIZE 128
+#include "optgen.h"
+OPTgen perset_optgen[LLC_SETS]; // per-set occupancy vectors; we only use 64 of these
+
+#define bitmask(l) (((l) == 64) ? (unsigned long long)(-1LL) : ((1LL << (l)) - 1LL))
+// Extract l-length bits from i to x
+#define bits(x, i, l) (((x) >> (i)) & bitmask(l))
+// Sample 64 ests per core, using bit ops to determine whether a set has been sampled
+#define SAMPLED_SET(set) (bits(set, 0, 6) == bits(set, ((unsigned long long)log2(LLC_SETS) - 6), 6))
+
+// Sampler to track 8x cache history for sampled sets
+// 2800 entris * 4 bytes per entry = 11.2KB
+#define SAMPLED_CACHE_SIZE 2800
+#define SAMPLER_WAYS 8
+#define SAMPLER_SETS SAMPLED_CACHE_SIZE/SAMPLER_WAYS
+vector<map<uint64_t, ADDR_INFO> > addr_history; // Sampler
+
+void CACHE::initialize_replacement()
+{
+    cout << "Initialize Glider replacement policy state" << endl;
+
+    for (int i = 0; i < LLC_SETS; i++)
+    {
+        for (int j = 0; j < LLC_WAYS; j++)
+        {
+            rrpv[i][j] = MAXRRIP;
+            signatures[i][j] = 0;
+            prefetched[i][j] = false;
+        }
+        // Initialize timestamps in sample all to 0
+        perset_mytimer[i] = 0;
+        perset_optgen[i].init(LLC_WAYS - 2);
+    }
+
+    addr_history.resize(SAMPLER_SETS);
+    for (int i = 0; i < SAMPLER_SETS; i++)
+    {
+        addr_history[i].clear();
+    }
+
+    demand_predictor = new Glider_Predictor();
+    prefetch_predictor = new Glider_Predictor();
+
+    cout << "Finished initializing Glider replacement policy state" << endl;
+}
+
+
+// This function is called when a tag is checked in the cache. The parameters passed are:
+// * triggering_cpu: the core index that initiated this fill
+// * instr_id: an instruction count that can be used to examine the program order of requests.
+// * set: the set that the fill occurred in.
+// * current_set: a pointer to the beginning of the set being accessed.
+// * ip: the address of the instruction that initiated the demand. If the packet is a prefetch from another level, this value will be 0.
+// * addr: the address of the packet. If this is the first-level cache, the offset bits are included. Otherwise, the offset bits are zero. If the cache was configured with `"virtual_prefetch": true`, this address will be a virtual address. Otherwise, this is a physical address.
+// * type: the result of `static_cast<std::underlying_type_t<access_type>>(v)`
+// The function should return the way index that should be evicted, or `this->NUM_WAY` to indicate that a bypass should occur.
+uint32_t CACHE::find_victim(uint32_t triggering_cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
+{
+    // Find victim from low RRPV value (7)
+    for (uint32_t i = 0; i < LLC_WAYS; i++)
+    {
+        if (rrpv[set][i] == MAXRRIP)
+        {
+            return i;
+        }
+    }
+
+    //If we cannot find a cache-averse line, we evict the oldest cache-friendly line
+    uint32_t max_rrip = 0;
+    int32_t lru_victim = -1;
+    for (uint32_t i=0; i<LLC_WAYS; i++)
+    {
+        if (rrpv[set][i] >= max_rrip)
+        {
+            max_rrip = rrpv[set][i];
+            lru_victim = i;
+        }
+    }
+
+    assert (lru_victim != -1);
+
+    // The predictor is trained negatively on LRU evictions
+    if (SAMPLED_SET(set))
+    {
+        // if(prefetched[set][lru_victim])
+        //     prefetch_predictor->decrement(signatures[set][lru_victim]);
+        // else
+        //     demand_predictor->decrement(signatures[set][lru_victim]);
+        demand_predictor->decrement(signatures[set][lru_victim]);
+
+        // Debug
+        // demand_predictor->print_all_weights();
+    }
+
+    return lru_victim;
+}
+
+void replace_addr_history_element(unsigned int sampler_set)
+{
+    uint64_t lru_addr = 0;
+    
+    for(map<uint64_t, ADDR_INFO>::iterator it=addr_history[sampler_set].begin(); it != addr_history[sampler_set].end(); it++)
+    {
+   //     uint64_t timer = (it->second).last_quanta;
+
+        if((it->second).lru == (SAMPLER_WAYS-1))
+        {
+            //lru_time =  (it->second).last_quanta;
+            lru_addr = it->first;
+            break;
+        }
+    }
+
+    addr_history[sampler_set].erase(lru_addr);
+}
+
+// Updates cache history with current value for the given sample set
+// Increments LRU for all cache history entries whose LRU value is less than currentVal
+void update_addr_history_lru(unsigned int sampler_set, unsigned int curr_lru)
+{
+    // Traverse cache history in given sample collection
+    for(map<uint64_t, ADDR_INFO>::iterator it=addr_history[sampler_set].begin(); it != addr_history[sampler_set].end(); it++)
+    {
+        if((it->second).lru < curr_lru)
+        {
+            (it->second).lru++;
+            assert((it->second).lru < SAMPLER_WAYS); 
+        }
+    }
+}
+
+// This function is called when a hit occurs or a miss is filled in the cache. The parameters passed are:
+// * triggering_cpu: the core index that initiated this fill
+// * set: the set that the fill occurred in.
+// * way: the way that the fill occurred in.
+// * full_addr: the address of the packet. If this is the first-level cache, the offset bits are included. Otherwise, the offset bits are zero. If the cache was configured with `"virtual_prefetch": true`, this address will be a virtual address. Otherwise, this is a physical address.
+// * ip: the address of the instruction that initiated the demand. If the packet is a prefetch from another level, this value will be 0.
+// * victim_addr: the address of the evicted block, if this is a miss. If this is a hit, the value is 0.
+// * type: the result of `static_cast<std::underlying_type_t<access_type>>(v)
+// * hit: indicates whether the access resulted in a cache hit
+// The function should return metadata that will be stored alongside the block.
+void CACHE::update_replacement_state(uint32_t triggering_cpu, uint32_t set, uint32_t way, uint64_t full_addr, uint64_t ip, uint64_t victim_addr, uint32_t type,
+                                     uint8_t hit)
+{
+    // Align address to cache line boundaries
+    full_addr = (full_addr >> 6) << 6;
+
+    if(type == PREFETCH)
+    {
+        if (!hit)
+            prefetched[set][way] = true;
+    }
+    else
+        prefetched[set][way] = false;
+
+    //Ignore writebacks
+    if (type == WRITEBACK)
+        return;
+
+    // If OPT selects current cache set for sampling
+    if (SAMPLED_SET(set))
+    {
+        // Get timestamp of current cache set modulo OPTGEN_VECTOR_SIZE
+        uint64_t curr_quanta = perset_mytimer[set] % OPTGEN_VECTOR_SIZE;
+        
+        // Get CRC hash modulo 256 to get sample tag
+        uint32_t sampler_set = (paddr >> 6) % SAMPLER_SETS; 
+        // Get sample set by ignoring cache line info then modulo sampler_sets
+        uint64_t sampler_tag = CRC(paddr >> 12) % 256;
+
+        assert(sampler_set < SAMPLER_SETS);
+
+        // This line has been used before. Since the right end of a usage interval is always 
+        //a demand, ignore prefetches
+        if((addr_history[sampler_set].find(sampler_tag) != addr_history[sampler_set].end()) && (type != PREFETCH))
+        {
+            unsigned int curr_timer = perset_mytimer[set];
+            // Account for time wraparound
+            if(curr_timer < addr_history[sampler_set][sampler_tag].last_quanta)
+               curr_timer = curr_timer + TIMER_SIZE;
+
+            bool wrap =  ((curr_timer - addr_history[sampler_set][sampler_tag].last_quanta) > OPTGEN_VECTOR_SIZE);
+            uint64_t last_quanta = addr_history[sampler_set][sampler_tag].last_quanta % OPTGEN_VECTOR_SIZE;
+
+            //and for prefetch hits, we train the last prefetch trigger PC
+            if( !wrap && perset_optgen[set].should_cache(curr_quanta, last_quanta))
+            {
+                // if(addr_history[sampler_set][sampler_tag].prefetched)
+                //     prefetch_predictor->increment(addr_history[sampler_set][sampler_tag].PC);
+                // else
+                //     demand_predictor->increment(addr_history[sampler_set][sampler_tag].PC);
+                demand_predictor->increment(addr_history[sampler_set][sampler_tag].PC);
+            }
+            else
+            {
+                //Train the predictor negatively because OPT would not have cached this line
+                // if(addr_history[sampler_set][sampler_tag].prefetched)
+                //     prefetch_predictor->decrement(addr_history[sampler_set][sampler_tag].PC);
+                // else
+                //     demand_predictor->decrement(addr_history[sampler_set][sampler_tag].PC);
+                demand_predictor->decrement(addr_history[sampler_set][sampler_tag].PC);
+            }
+
+            // predictor_demand->print_all_weights();
+
+            //Some maintenance operations for OPTgen
+            perset_optgen[set].add_access(curr_quanta);
+            update_addr_history_lru(sampler_set, addr_history[sampler_set][sampler_tag].lru);
+
+            //Since this was a demand access, mark the prefetched bit as false
+            addr_history[sampler_set][sampler_tag].prefetched = false;
+        }
+        // This is the first time we are seeing this line (could be demand or prefetch)
+        else if(addr_history[sampler_set].find(sampler_tag) == addr_history[sampler_set].end())
+        {
+            // Find a victim from the sampled cache if we are sampling
+            if(addr_history[sampler_set].size() == SAMPLER_WAYS) 
+                replace_addr_history_element(sampler_set);
+
+            assert(addr_history[sampler_set].size() < SAMPLER_WAYS);
+
+            //Initialize a new entry in the sampler
+            addr_history[sampler_set][sampler_tag].init(curr_quanta);
+            //If it's a prefetch, mark the prefetched bit;
+            if(type == PREFETCH)
+            {
+                addr_history[sampler_set][sampler_tag].mark_prefetch();
+                perset_optgen[set].add_prefetch(curr_quanta);
+            }
+            else
+                perset_optgen[set].add_access(curr_quanta);
+            update_addr_history_lru(sampler_set, SAMPLER_WAYS-1);
+        }
+        //This line is a prefetch
+        else
+        {
+            assert(addr_history[sampler_set].find(sampler_tag) != addr_history[sampler_set].end());
+            //if(hit && prefetched[set][way])
+            uint64_t last_quanta = addr_history[sampler_set][sampler_tag].last_quanta % OPTGEN_VECTOR_SIZE;
+            if (perset_mytimer[set] - addr_history[sampler_set][sampler_tag].last_quanta < 5*NUM_CORE) 
+            {
+                if(perset_optgen[set].should_cache(curr_quanta, last_quanta))
+                {
+                    if(addr_history[sampler_set][sampler_tag].prefetched)
+                        prefetch_predictor->increment(addr_history[sampler_set][sampler_tag].PC);
+                    else
+                       demand_predictor->increment(addr_history[sampler_set][sampler_tag].PC);
+                }
+            }
+
+            //Mark the prefetched bit
+            addr_history[sampler_set][sampler_tag].mark_prefetch(); 
+            //Some maintenance operations for OPTgen
+            perset_optgen[set].add_prefetch(curr_quanta);
+            update_addr_history_lru(sampler_set, addr_history[sampler_set][sampler_tag].lru);
+        }
+        // Get Hawkeye's prediction for this line
+        bool new_prediction = demand_predictor->get_prediction (ip);
+        if (type == PREFETCH)
+            new_prediction = prefetch_predictor->get_prediction (ip);
+        // Update the sampler with the timestamp, PC and our prediction
+        // For prefetches, the PC will represent the trigger PC
+        addr_history[sampler_set][sampler_tag].update(perset_mytimer[set], ip, new_prediction);
+        addr_history[sampler_set][sampler_tag].lru = 0;
+        //Increment the set timer
+        perset_mytimer[set] = (perset_mytimer[set]+1) % TIMER_SIZE;
+    }
+
+    bool new_prediction = demand_predictor->get_prediction (ip);
+    if (type == PREFETCH)
+        new_prediction = prefetch_predictor->get_prediction (ip);
+
+    signatures[set][way] = ip;
+
+    // Update RRIP value for the current set and way
+    if (prediction == Prediction::Low)
+    {
+        rrpv[set][way] = MAXRRIP;
+    }
+    else if (prediction == Prediction::Medium)
+    {
+        rrpv[set][way] = MIDRRIP;
+    }
+    // High Priority
+    else
+    {
+        rrpv[set][way] = 0;
+        // Access is a miss, check RRPV values of rows are sturated
+        if (!hit)
+        {
+            bool isMaxVal = false;
+            for (uint32_t i = 0; i < LLC_WAYS; i++)
+            {
+                if (rrpv[set][i] == MIDRRIP - 1)
+                {
+                    isMaxVal = true;
+                }
+            }
+
+            // 如果没有达到最大值，则使所有友好cache行老化（增加其RRIP值）
+            for (uint32_t i = 0; i < LLC_WAYS; i++)
+            {
+                if (!isMaxVal && rrpv[set][i] < MIDRRIP - 1)
+                {
+                    rrpv[set][i]++;
+                }
+            }
+        }
+        // Set RRIP of current way to 0 (recently used)
+        rrpv[set][way] = 0;
+    }
+}
+
+void CACHE::replacement_final_stats() {}
